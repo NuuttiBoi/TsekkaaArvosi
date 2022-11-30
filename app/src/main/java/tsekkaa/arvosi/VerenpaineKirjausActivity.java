c088f059ea1c32c18b21470123bee848da6cb948
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2022-11-30 13:12:19 +0200

    Verenpaineen kirjaus activity layout muutoksia

--- app/src/main/java/tsekkaa/arvosi/VerenpaineKirjausActivity.java
+++ app/src/main/java/tsekkaa/arvosi/VerenpaineKirjausActivity.java
@@ -2,6 +2,7 @@ package tsekkaa.arvosi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,8 +12,9 @@ public class VerenpaineKirjausActivity extends AppCompatActivity {
     public final static String EXTRA_ALAPAINE = "com.example.excercise41.ALAPAINE";
     public final static String EXTRA_SYKE = "com.example.excercise41.SYKE";
     TextView kirjausTextView, alaPaineTextView, ylaPaineTextView, sykeTextView;
-    EditText ylaPaineEditText, alaPaineEditText, sykeEditText;
+    EditText ylaPaineEditText, alaPaineEditText, sykeEditText, pvmEditText, aikaEditText;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +28,8 @@ public class VerenpaineKirjausActivity extends AppCompatActivity {
         this.ylaPaineEditText = findViewById(R.id.ylaPaineEditText);
         this.alaPaineEditText = findViewById(R.id.alaPaineEditText);
         this.sykeEditText = findViewById(R.id.sykeEditText);
-
+        this.pvmEditText = findViewById(R.id.pvmEditTextDate);
+        this.aikaEditText = findViewById(R.id.aikaEditTextTime);
 
     }
 }
