b527fc70e9207e74da05c264e30afc2dba1a1979
tiitu3 <tia.eranen@metropolia.fi>
2022-11-30 14:37:31 +0200

    Asetukset otsikko, kirjaudu ulos näppäin. yritys laittaa tesktiä asetukset.javaan.

--- app/src/main/java/tsekkaa/arvosi/Asetukset.java
+++ app/src/main/java/tsekkaa/arvosi/Asetukset.java
@@ -1,4 +1,32 @@
 package tsekkaa.arvosi;
 
-public class Asetukset {
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Asetukset extends AppCompatActivity {
+    private Button kirjauduUlos;
+
+    public Asetukset() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asetukset);
+
+        this.kirjauduUlos = findViewById(R.id.kirjauduUlosButton);
+    }
+
+    /**public void kirjauduUlosButtonpressed(View asetusnakyma){
+        Intent intentKirjauduUlos = new Intent(this, MessageActivity.class);
+        startActivity(intentKirjauduUlos); teksti Olet kirjautunut ulos sovelluksesta
+    }**/
 }
+
+
