59bfab4c36fd13795688596152aea96169935b22
RiQhy <riku.nokelainen@metropolia.fi>
2022-11-30 14:07:10 +0200

    Buttonien yhteys nextActivityihin

--- app/src/main/java/tsekkaa/arvosi/MainActivity.java
+++ app/src/main/java/tsekkaa/arvosi/MainActivity.java
@@ -18,9 +18,26 @@ public class MainActivity extends AppCompatActivity {
 
         this.verenpaineButton =findViewById(R.id.verenpaineButton);
     }
+    //Checks if the selected button is pressed and sends the user to the selected page/ activity
     public void vPaineButtonPressed(View v){
         Intent vPaine = new Intent(this, VerenpaineKirjausActivity.class);
         startActivity(vPaine);
     }
+    public void asetuksetButtonPressed(View v){
+        Intent asetukset = new Intent(this, Asetukset.class);
+        startActivity(asetukset);
+    }
+    public void VerenHappiPitoisuusButtonPressed(View v){
+        Intent VHPitoisuus = new Intent(this, VerenHappipitoisuus.class);
+        startActivity(VHPitoisuus);
+    }
+    public void VerenSokeriButtonPressed(View v){
+        Intent verensokeri = new Intent(this, VerenSokeri.class);
+        startActivity(verensokeri);
+    }
+    //public void ButtonPressed(View v){
+    //    Intent  = new Intent(this, .class);
+    //    startActivity();
+    //}
 
 }
