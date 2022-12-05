419c9f4141ddde0d2ea1ffca99d140206985c9bb
RiQhy <riku.nokelainen@metropolia.fi>
2022-12-05 09:34:12 +0200

    Backbutton luotu verensokeriactivityssä

--- app/src/main/java/tsekkaa/arvosi/VerenSokeriActivity.java
+++ app/src/main/java/tsekkaa/arvosi/VerenSokeriActivity.java
@@ -2,17 +2,26 @@ package tsekkaa.arvosi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class VerenSokeriActivity extends AppCompatActivity {
-    private Button tallenna;
+    //luo buttonin
+    private Button tallennabutton, backbutton, siirrybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_veren_sokerin_kirjaus);
 
-        this.tallenna = findViewById(R.id.tallennusButton);
+        this.tallennabutton = findViewById(R.id.tallennusButton);
+        this.backbutton = findViewById(R.id.Backbutton);
+        this.siirrybutton = findViewById(R.id.siirryKalenteriinbutton);
+    }
+    public void backButtonPressed(View v){
+        Intent takaisin = new Intent(this, MainActivity.class);
+        startActivity(takaisin);
     }
 }
