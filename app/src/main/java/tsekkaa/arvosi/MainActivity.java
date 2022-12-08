ddcc810f9da7c86690f280250f38a96b874662c8
RiQhy <riku.nokelainen@metropolia.fi>
2022-12-08 18:07:50 +0200

    Listview toimii!!! c(OoOc)

--- app/src/main/java/tsekkaa/arvosi/MainActivity.java
+++ app/src/main/java/tsekkaa/arvosi/MainActivity.java
@@ -4,44 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import java.text.*;
+import android.widget.ListView;
 
+import java.text.*;
+//Riku Nokelainen
+//Frontpage
 public class MainActivity extends AppCompatActivity {
 
-    //creates button
-    private Button verenpaineButton, verenHappipitoisuusButton, verensokeriButton, asetuksetButton, testButton;
+    //Creates buttons
+    private Button testButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //finds a button from layout with findViewById(R.id.(wanted thing)) command and sets the found value to the created button
-        this.verenpaineButton = findViewById(R.id.verenpaineButton);
-        this.verenHappipitoisuusButton = findViewById(R.id.verenHappiPitoisuus);
-        this.verensokeriButton = findViewById(R.id.verenSokeri);
-        this.asetuksetButton = findViewById(R.id.asetukset);
+        //Finds a button from layout with findViewById(R.id.(wanted thing)) command and sets the found value to the created button
         this.testButton = findViewById(R.id.testiButton);
+        ListView lv = findViewById(R.id.activitySelection);
+
+        lv.setAdapter(new ArrayAdapter<NextActivity>(
+                this,  //context (activity instance)
+                android.R.layout.simple_list_item_1,
+                ActivitySelectionSingleton.getInstance().getActivity()) //array of data
+        );
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent verenpaine = new Intent(MainActivity.this, VerenpaineKirjausActivity.class);
+                Intent verenhappipitoisuus = new Intent(MainActivity.this, VerenHappipitoisuusActivity.class);
+                Intent verensokeri = new Intent(MainActivity.this, VerenSokeriActivity.class);
+                Intent asetukset = new Intent(MainActivity.this, Asetukset.class);
+                Intent testi = new Intent(MainActivity.this, TestiActivity.class);
+                if(i == 0){
+                    startActivity(verenpaine);
+                } else if(i == 1){
+                    startActivity(verenhappipitoisuus);
+                } else if(i == 2){
+                    startActivity(verensokeri);
+                } else if(i == 3){
+                    startActivity(asetukset);
+                } else{
+                    startActivity(testi);
+                }
+            }
+        });
 }
+
     //Checks if the selected button is pressed and sends the user to the selected page/ activity
-    public void vPaineButtonPressed(View v){
-        Intent vPaine = new Intent(this, VerenpaineKirjausActivity.class);
-        startActivity(vPaine);
-    }
-    public void vhpButtonPressed(View v){
-        Intent VHP = new Intent(this, VerenHappipitoisuusActivity.class);
-        startActivity(VHP);
-    }
-    public void vsButtonPressed(View v){
-        Intent VeSo = new Intent(this, VerenSokeriActivity.class);
-        startActivity(VeSo);
-    }
-    public void settingsButtonPressed(View v){
-        Intent settings = new Intent(this, Asetukset.class);
-        startActivity(settings);
-    }
     //muistakaa laittaa class tohon alle huutomerkin tilalle. Menee samallalailla ku noissa muissa
     public void testButtonPressed(View v){
         Intent test = new Intent(this, TestiActivity3.class);
