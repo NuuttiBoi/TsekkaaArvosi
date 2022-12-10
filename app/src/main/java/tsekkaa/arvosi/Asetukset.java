53fcc2f71b8e0b7e985a4efec1ae138887afb543
tiitu3 <tia.eranen@metropolia.fi>
2022-12-10 11:35:33 +0200

    Switcheistä takas buttoneihin

--- app/src/main/java/tsekkaa/arvosi/Asetukset.java
+++ app/src/main/java/tsekkaa/arvosi/Asetukset.java
@@ -1,46 +1,50 @@
 package tsekkaa.arvosi;
 
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Asetukset extends AppCompatActivity {
-    private Button kirjauduUlos;
     private Button ilmoitukset;
     private Button teema;
     private Button omatTiedot;
     private Button backbutton;
 
+
     public Asetukset() {
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asetukset);
 
-        this.kirjauduUlos = findViewById(R.id.kirjauduUlosButton);
         this.ilmoitukset = findViewById(R.id.ilmoituksetButton);
         this.teema = findViewById(R.id.teemaButton);
-        this.omatTiedot =findViewById(R.id.omatTiedotButton);
+        this.omatTiedot = findViewById(R.id.omatTiedotButton);
         this.backbutton = findViewById(R.id.BackButton);
-
     }
 
-    /** Päätetty jättää sisään ja uloskirjautuminen pois
-     * public void kirjauduUlosButtonpressed(View asetusnakyma){
-        Intent intentKirjauduUlos = new Intent(this, MessageActivity.class);
-        startActivity(intentKirjauduUlos); teksti Olet kirjautunut ulos sovelluksesta
-    }**/
+    public void ilmoituksetButtonPressed(View v){
 
+    }
 
     public void teemaButtonPressed(View v){
         Intent teema = new Intent(this, TeemaActivity.class);
         startActivity(teema);
+    }
+
+    public void omatTiedotButtonPressed(View v){
+
     }
 
     public void backButtonPressed(View v){
