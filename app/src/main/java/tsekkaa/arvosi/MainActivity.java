866c97d7e37c5313976a2ca19e0fd392a2d670c4
RiQhy <riku.nokelainen@metropolia.fi>
2022-12-13 16:05:58 +0200

    kalenteri button poistettu main activitystä

--- app/src/main/java/tsekkaa/arvosi/MainActivity.java
+++ app/src/main/java/tsekkaa/arvosi/MainActivity.java
@@ -56,15 +56,15 @@ public class MainActivity extends AppCompatActivity {
                 Intent verensokeri = new Intent(MainActivity.this, VerenSokeriKirjausActivity.class);
                 Intent asetukset = new Intent(MainActivity.this, Asetukset.class);
                 //Intent testi = new Intent(MainActivity.this, TestiActivity.class);
-                if(i == 0){
+                if (i == 0) {
                     startActivity(verenpaine);
-                } else if(i == 1){
+                } else if (i == 1) {
                     startActivity(verenhappipitoisuus);
-                } else if(i == 2){
+                } else if (i == 2) {
                     startActivity(verensokeri);
-                } else if(i == 3){
+                } else if (i == 3) {
                     startActivity(asetukset);
-                } else{
+                } else {
                     //startActivity(testi);
                 }
             }
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Use the device's default theme if no other theme is selected or the app is started for the first time
         sharedPref = getSharedPreferences("Asetukset", Context.MODE_PRIVATE);
-        if(sharedPref.getBoolean("Oletusteema", true)) {
+        if (sharedPref.getBoolean("Oletusteema", true)) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
         } else {
             //If the user has unselected the default theme in the settings, use the theme that was last saved
@@ -82,35 +82,5 @@ public class MainActivity extends AppCompatActivity {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
         }
-}
-
-    /**
-     * Takes you to calender view
-     * @param v Button that is pressed
-     */
-    //Checks if the selected button is pressed and sends the user to the selected page/ activity
-    public void KalenteriButtonPressed(View v) {
-        Intent kalenteri = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            kalenteri = new Intent(this, KalenteriActivity.class);
-        }
-        startActivity(kalenteri);
-        //zoom in
-        //overridePendingTransition(R.anim.zoom_in, R.anim.static_anim);
-
-        //zoom out
-        //overridePendingTransition(R.anim.static_anim, R.anim.zoom_out);
-
-        //slide up
-        //overridePendingTransition(R.anim.slide_from_bottom, R.anim.slide_to_top);
-
-        //slide down
-        //overridePendingTransition(R.anim.slide_from_top, R.anim.slide_to_bottom);
-
-        //slide to the left
-        //overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
-
-        //slide to the right
-        overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
     }
 }
